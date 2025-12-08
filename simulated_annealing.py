fadf78ec8cd7d9f1004ab9d9074e900af6288dc5
Amar Shah <amar.shah1999@gmail.com>
2025-12-08 21:58:18 +0000

    Various improvements
    - Reworked app.py to do all the data loading and display the graph as a fragment
    - Added new objective terms to the annealer
    - Made annealer work for both teams
    - turned simulated_annealing file into just helper funcs

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,34 +1,11 @@
-# Annealer
-from typing import Any
-import random
-import math
-from kloppy import skillcorner
 import pandas as pd
-import numpy as np
-from databallpy import get_game_from_kloppy, get_saved_game
 from databallpy.features.pitch_control import get_pitch_control_single_frame
-from databallpy.visualize import save_tracking_video, plot_soccer_pitch, plot_tracking_data
-from kloppy import skillcorner, sportec
-import matplotlib.pyplot as plt
+from databallpy.visualize import plot_soccer_pitch, plot_tracking_data
 from matplotlib.colors import LinearSegmentedColormap
-import time
 from copy import deepcopy
+
 from annealer import Annealer
 
-match_id = 1886347
-frame_idx = 210
-
-tracking_data_github_url = f"https://media.githubusercontent.com/media/SkillCorner/opendata/master/data/matches/{match_id}/{match_id}_tracking_extrapolated.jsonl"
-meta_data_github_url = f"https://raw.githubusercontent.com/SkillCorner/opendata/master/data/matches/{match_id}/{match_id}_match.json"
-
-dataset = skillcorner.load(
-    meta_data=meta_data_github_url,
-    raw_data=tracking_data_github_url,
-    # Optional Parameters
-    coordinates="skillcorner",  # or specify a different coordinate system
-    # sample_rate=(1 / 2),  # changes the data from 10fps to 5fps
-    limit=1000,  # only load the first 100 frames
-)
 
 def generate_fig(game, frame_idx):
     frame = game.tracking_data[game.tracking_data['frame']==frame_idx].iloc[0]
@@ -52,19 +29,16 @@ def generate_fig(game, frame_idx):
     )
     return fig,ax
 
-#actual script
-game = get_game_from_kloppy(dataset)
-game.tracking_data.add_velocity(game.get_column_ids() + ["ball"], allow_overwrite=True)
 
+def run_annealer(annealer: Annealer, game):
+    best_solution, best_score = annealer.anneal() 
+    new_game = deepcopy(game)
+    new_game.tracking_data = pd.DataFrame(best_solution).transpose().reset_index()
+    return new_game, best_score
 
-latest_frame = game.tracking_data[game.tracking_data['frame']==frame_idx].iloc[0]
-
-best_solution, best_score = Annealer(game, latest_frame).anneal() 
-new_game = deepcopy(game)
-new_game.tracking_data = pd.DataFrame(best_solution).transpose().reset_index()
-def get_figs():
-    original = generate_fig(game, frame_idx)
-    new_positions = generate_fig(new_game, frame_idx)
+def get_figs(new_game_obj, game, selected_frame_idx):
+    original = generate_fig(game, selected_frame_idx)
+    new_positions = generate_fig(new_game_obj, selected_frame_idx)
     return original, new_positions
 
 
